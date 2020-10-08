0a600b44ceb2f3d38165bcc60615aa1148b0001d
Daniel Smanioto <daniel.smanioto@gmail.com>
2020-10-07 23:46:38 -0300

    Swagger configurations refect

--- src/main/java/com/dsmanioto/address/config/SwaggerConfig.java
+++ src/main/java/com/dsmanioto/address/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.dsmanioto.address.config;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig  {
 
