e5c80d5d435c70b1b5474d996ae7d142682156e6
Daniel Smanioto <daniel.smanioto@gmail.com>
2020-10-12 02:26:03 -0300

    Refatorando construtor com observação do sonarlint

--- src/main/java/com/dsmanioto/address/authentication/PasswordEncrypter.java
+++ src/main/java/com/dsmanioto/address/authentication/PasswordEncrypter.java
@@ -4,6 +4,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncrypter {
 
+    private PasswordEncrypter() {}
+
     public static String encrypt(String password) {
         return new BCryptPasswordEncoder().encode(password);
     }
